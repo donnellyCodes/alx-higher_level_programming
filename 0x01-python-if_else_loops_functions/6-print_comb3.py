d20adaa5cbacfbb8eb1ec3cd659510837a9595b2
donnelly19 <donamaitsa@gmail.com>
2023-09-06 11:41:43 -0700

    combination of digits fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i == 0:
-            print("{:d}{:d}".format(i, j), end=", ")
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
         else:
-            print("{:d}{:d} ".format(i, j), end=", ")
+            print("{:d}{:d}".format(i, j), end=", ")
