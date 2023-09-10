d46c17f0135b78d2395371e3456b3dfa035bc0e8
donnelly19 <donamaitsa@gmail.com>
2023-09-10 09:06:19 -0700

    printing the number and the list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# sys is used to access list of command line arguments passed to it
+import sys
+if __name__ == '__main__':
+    # to determine number of arguments
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("O arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{argc} arguments:")
+        for i in range(1, argc + 1):
+            print(f"{i}: {sys.argv[i]}")
