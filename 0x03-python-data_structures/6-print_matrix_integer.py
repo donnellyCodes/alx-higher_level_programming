b608452fcae21b2d1af2688a0f81e24f777a9c06
donnelly19 <donamaitsa@gmail.com>
2023-09-12 11:53:35 -0700

    fixing error on printing matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
-                print("{:d}".format(row[i]))
+                print("{:d}".format(row[i]), end="")
             else:
                 print("{:d}".format(row[i]), end=" ")
+        print()
