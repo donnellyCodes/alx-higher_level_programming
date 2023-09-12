260cee4c739cc83fb5fc7bdcf0ede15fbd79ab19
donnelly19 <donamaitsa@gmail.com>
2023-09-12 11:28:29 -0700

    printing matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
