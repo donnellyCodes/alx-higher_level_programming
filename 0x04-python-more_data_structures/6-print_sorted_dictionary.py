642e7b94ed6e92f24ceb5b2f0600a6276680b5bc
donnelly19 <donamaitsa@gmail.com>
2023-09-14 04:44:21 -0700

    fixing typo error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     # this function only sorts first level keys in a dictionary
     for key in sorted(a_dictionary):
-        print(key, ':', a_dictionary[key])
+        print("{:s}: {}".format(key, a_dictionary[key]))
