01a4d8252391fa5e42e2b653e56d65579a094c87
donnelly19 <donamaitsa@gmail.com>
2024-04-29 13:40:31 -0700

    fixing error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,6 +6,3 @@ USE hbtn_0c_0;
 -- converting first_table to UTF8
 ALTER TABLE first_table
 	CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- converting name field in first_table to UTF8
-ALTER TABLE first_table CHANGE name VARCHAR(256)
-	CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
