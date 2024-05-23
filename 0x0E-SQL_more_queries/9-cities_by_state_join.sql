667dc3013cd0e49e740dfdb6fcd9685648384fc9
donnelly19 <donamaitsa@gmail.com>
2024-05-23 03:05:59 -0700

    fixing error

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 -- results is sorted in ascending order
 SELECT cities.id, cities.name, states.name
 FROM cities AS cities
-INNER JOIN states as states
+INNER JOIN states AS states
 ON cities.id = states.id
 ORDER BY cities.id;
