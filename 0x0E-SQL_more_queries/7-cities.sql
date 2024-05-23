f6766aa9e3501fafc4311a7c786ac623ead753ed
donnelly19 <donamaitsa@gmail.com>
2024-05-23 02:43:25 -0700

    updating cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	PRIMARY KEY(id),
 	id INTEGER NOT NULL AUTO_INCREMENT,
 	state_id INTEGER NOT NULL,
+	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY(state_id)
 	REFERENCES hbtn_0d_usa.states(id)
 );
