4e3b306a579fefcc4382ebc751860789df842d68
donnelly19 <donamaitsa@gmail.com>
2024-05-23 04:19:03 -0700

    editing scripts

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT g.name AS genre,
 	COUNT(*) AS number_of_shows
  FROM tv_genres AS g
-	INNER JOIN tv_shows_genres AS t
+	INNER JOIN tv_show_genres AS t
 	ON g.id = t.genre.id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
