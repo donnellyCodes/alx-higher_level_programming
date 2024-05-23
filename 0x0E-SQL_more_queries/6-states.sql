097b2ab7dee68e458f3eca8f366fadca0d481825
donnelly19 <donamaitsa@gmail.com>
2024-05-23 02:40:12 -0700

    fixing error 2

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creating hbtn_0d_usa database and states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	PRIMARY KEY(id),
 	id INTEGER NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- creating cities table in hbtn_0d_usa database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	PRIMARY KEY(id),
 	id INTEGER NOT NULL AUTO_INCREMENT,
 	state_id INTEGER NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES states(id)
+	FOREIGN KEY(state_id)
+	REFERENCES hbtn_0d_usa.states(id)
 );
