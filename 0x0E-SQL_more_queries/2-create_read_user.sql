2293d65b3012aaca586a9e9f1d7598548c69b70a
donnelly19 <donamaitsa@gmail.com>
2024-05-23 01:35:35 -0700

    fixing error

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- creating a database and a new user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY user_0d_2_pwd';
+IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 ON hbtn_0d_2.*
 TO 'user_0d_2'@'localhost';
