10b98ddd93dbdbf51da5a5c8ccf6f60102461f4a
donnelly19 <donamaitsa@gmail.com>
2024-05-23 03:10:03 -0700

    fixing error2

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- listing all cities in hbtn_0d_usa database
 -- results is sorted in ascending order
-SELECT cities.id, cities.name, states.name
-FROM cities AS cities
-INNER JOIN states AS states
-ON cities.id = states.id
-ORDER BY cities.id;
+SELECT c.id, c.name, s.name
+FROM cities AS c
+INNER JOIN states AS s
+ON c.state_id = s.id
+ORDER BY c.id;
