11383c571d777ed9d706b58f6e2574c03315a073
donnelly19 <donamaitsa@gmail.com>
2024-06-17 05:31:10 -0700

    fixing error task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
-function findSecondBiggest(args) {
-	let max = - Infinity, secondMax = -Infinity;
-	args.forEach((args) => {
-		const num = parseInt(arg, 10);
-		if (num > max) {
-			[secondMax, max] = [max, num];
-		} else if (num > secondMax && num < max) {
-			secondMax = num;
-		}
-	});
-	return secondMax;
-}
-const args = process.argv.slice(2);
-if (args.length < 2) {
+if (process.argv.length <= 3) {
 	console.log(0);
 } else {
-	console.log(findSecondBiggest(args));
+	const args = process.argv.map(Number)
+	.slice(2, process.argv.length)
+	.sort((a, b) => a - b);
+	console.log(args[args.length - 2]);
 }
