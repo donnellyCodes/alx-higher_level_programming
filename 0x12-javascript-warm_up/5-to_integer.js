50163ccc89d4b040ee38f4295e38ba5c0f70b427
donnelly19 <donamaitsa@gmail.com>
2024-06-17 03:42:07 -0700

    converting argument to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const [firstArg] = process.argv.slice(2);
+const number = parseInt(firstArg, 10); // convert to integer
+if (isNaN(number)) {
+	console.log('Not a number');
+} else {
+	console.log(`My number: ${number}`);
+}
