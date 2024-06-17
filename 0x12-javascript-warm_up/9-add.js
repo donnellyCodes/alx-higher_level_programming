fe09aca4be6e0ec198d1c6c95d81fe141dd3c145
donnelly19 <donamaitsa@gmail.com>
2024-06-17 04:58:16 -0700

    adding two numbers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add(a, b) {
+	return a + b;
+}
+const [firstArg, secondArg] = process.argv.slice(2);
+const firstInt = parseInt(firstArg, 10);
+const secondInt = parseInt(secondArg, 10);
+console.log(add(firstInt, secondInt));
