e57e2be3ae37912e30649fe41cccd152c2e50d79
donnelly19 <donamaitsa@gmail.com>
2024-06-17 04:02:22 -0700

    printing multiple times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const [firstArg] = process.argv.slice(2);
+const count = parseInt(firstArg, 10);
+if (isNaN(count)) {
+	console.log('Missing number of occurences');
+} else {
+	for (let i = 0; i < count; i++) {
+		console.log('C is fun');
+	}
+}
