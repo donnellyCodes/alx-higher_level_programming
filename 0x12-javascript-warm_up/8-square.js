3c28d6767e74a8ec0eb3842f3084f09bd3322ae4
donnelly19 <donamaitsa@gmail.com>
2024-06-17 04:13:12 -0700

    drawing a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const [firstArg] = process.argv.slice(2);
+const size = parseInt(firstArg, 10);
+if (isNaN(size)) {
+	console.log('Missing size');
+} else {
+	const line = 'X' .repeat(size);
+	for (let i = 0; i < size; i++) {
+		console.log(line);
+	}
+}
