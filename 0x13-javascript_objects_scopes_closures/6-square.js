7bb20090f0d30f6f714115a879bc26acb9cc6884
donnelly19 <donamaitsa@gmail.com>
2024-06-20 02:21:34 -0700

    fixing error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square,js') {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
