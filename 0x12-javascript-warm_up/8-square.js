2218c29e709aa0bc57ca2b6639380f23447d2ed5
donnelly19 <donamaitsa@gmail.com>
2024-06-19 23:47:42 -0700

    fixing code task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const size = parseInt(firstArg, 10);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  const line = 'X' .repeat(size);
+  const line = 'X'.repeat(size);
   for (let i = 0; i < size; i++) {
     console.log(line);
   }
