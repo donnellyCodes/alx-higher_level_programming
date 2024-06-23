f74814f47fdb53977c007f36ad8bd502ff30a0d1
donnelly19 <donamaitsa@gmail.com>
2024-06-23 03:15:13 -0700

    adding files

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/python3
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'"
+            .format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
