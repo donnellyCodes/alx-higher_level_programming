ada37fa89fa2100e1ff7275ec028c95a7d7474f5
donnelly19 <donamaitsa@gmail.com>
2024-06-23 22:46:36 -0700

    fixing code error task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], host="Localhost", port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    data = cur.fetchall()
-    for row in data:
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
     cur.close()
