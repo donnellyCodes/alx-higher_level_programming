f8ed6c648123e0cb03cc1b36b91612771f263273
donnelly19 <donamaitsa@gmail.com>
2024-06-24 01:33:14 -0700

    creating City class and writing a script that lists all cities

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+Defines a class City
+"""
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
-class State(Base):
-    __tablename__ = 'states'
+
+class City(Base):
+    """city module"""
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
