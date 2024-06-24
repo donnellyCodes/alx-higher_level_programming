adb15b35d5c23c39aab3050e032ca843c9f3c80a
donnelly19 <donamaitsa@gmail.com>
2024-06-23 22:58:24 -0700

    fixing pycodestyle task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
- script that lists all states with a name starting with N from the database hbtn_0e_0_usa
+ script that lists all states with names starting with N from the database
+ hbtn_0e_0_usa
  It takes three arguments username, password and database name
  """
 
@@ -8,7 +9,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
