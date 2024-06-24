2c005368e53ab53e0e2f002c84968529bc6090be
donnelly19 <donamaitsa@gmail.com>
2024-06-23 23:13:30 -0700

    adding documentation and fixing pycodestyle task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,22 @@
-#!/usr/python3
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+It has 4 arguments username, password, database name and state name searched
+"""
 
-import sys
 import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'"
-            .format(sys.argv[4]))
-    [print(state) for state in cur.fetchall()]
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE '{:s}' ORDER BY \
+                id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
