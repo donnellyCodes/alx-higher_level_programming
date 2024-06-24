7b4b97964848c7502a5d471564d8adfe84a11e7a
donnelly19 <donamaitsa@gmail.com>
2024-06-24 00:55:00 -0700

    changing the name of state using an id

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""
+a script that changes the name of a State object from database hbtn_0e_6_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3])
-            pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     up_state = session.query(State).filter_by(id=2).first()
     up_state.name = 'New Mexico'
     session.commit()
+    session.close()
