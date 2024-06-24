63990833a15254e873a49f60cb627f0677fe0c2c
donnelly19 <donamaitsa@gmail.com>
2024-06-24 01:04:18 -0700

    fixing error task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,9 +16,8 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for states in session.query(State):
-        if 'a' in states.name:
-            session.delete(states)
+    states = session.query(State).filter(State.name.like('%a%'))
+    for state in states:
+        session.delete(state)
     session.commit()
     session.close()
