9deb8b0ec5313b669e008f0ec9d4a96391d9195a
donnelly19 <donamaitsa@gmail.com>
2024-06-23 23:23:23 -0700

    a safe file protected from mysql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 
-import sys
+"""
+a script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections
+"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s;", (sys.argv[4],))
-    [print(states) for states in cur.fetchall()]
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
+                (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
