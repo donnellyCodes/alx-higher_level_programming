a177443df1f8049f8cb284914d6d760ca17c9cd6
donnelly19 <donamaitsa@gmail.com>
2024-07-04 05:13:57 -0700

    fixing error2

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes URL, sends a POST request to passed URL
-curl -sX POST -d "email=test@gmail.com&I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
