c6356cdd8d016635b5940ecfd64c79dea6155f38
donnelly19 <donamaitsa@gmail.com>
2024-07-05 03:02:39 -0700

    fetching URL using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+if __name__ == '__main__':
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf-8 content: {}".format(the_page.decode('utf-8')))
