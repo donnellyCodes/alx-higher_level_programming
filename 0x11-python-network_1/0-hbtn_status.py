0b9b24c29dd1fd4616f3ed1efda88c537d7367b7
donnelly19 <donamaitsa@gmail.com>
2024-07-05 03:09:55 -0700

    fixing error0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,10 @@ import urllib.request
 
 if __name__ == '__main__':
 
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
-        print('Body response:')
+        print("Body response:")
         print("\t- type: {}".format(type(the_page)))
         print("\t- content: {}".format(the_page))
-        print("\t- utf-8 content: {}".format(the_page.decode('utf-8')))
+        print("\t- utf-8 content: {}".format(the_page.decode("utf-8")))
