ef24fe5f77f90f6b668ca5f5f04381f59a19c920
donnelly19 <donamaitsa@gmail.com>
2024-07-05 19:40:01 -0700

    fixing variable name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
 
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
-        the_page = response.read()
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
-        print("\t- utf-8 content: {}".format(the_page.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf-8 content: {}".format(body.decode("utf-8")))
