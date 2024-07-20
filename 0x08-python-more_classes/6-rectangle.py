81d4d9820c456275557a4c362990691564baa54c
donnelly19 <donamaitsa@gmail.com>
2024-07-20 14:13:41 -0700

    changing representation

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,6 @@
 
 class Rectangle:
     """initialzing number of instances"""
-
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""
+Class rectangle
+"""
+
 
 class Rectangle:
     """"defines a rectangle by it's width and height"""
-
-
     number_of_instances = 0
     print_symbol = "#"
 
@@ -44,22 +46,31 @@ class Rectangle:
                 self.__height = value
 
         def area(self):
-            return self.width * self.height
-        
+            return (self.__width * self.__height)
+
         def perimeter(self):
-            if self.width == 0 or self.height == 0:
+            if (self.__width == 0 or self.__height == 0):
                 return 0
             else:
-                return 2 * (self.width + self.height)
+                return (2 * (self.__width + self.__height))
 
         def __str__(self):
-            if self.width == 0 or self.height == 0:
-                return ""
-            else:
-                return "/n".join([Rectangle.print_symbol * self.width] * self.height)
+            if self.__width == 0 or self.__height == 0:
+                return ("")
+
+            rectangle = []
+            for i in range(self.__height):
+                [rectangle.append(str(self.print_symbol))
+                 for j in range(self.__width)]
+                if i != self.__height - 1:
+                    rectangle.append("\n")
+            return ("".join(rectangle))
 
         def __repr__(self):
-            return f"Rectangle({self.width}, {self.height})"
+            rectangle = "Rectangle(" + str(self.__width)
+            rectangle = rectangle + ", " + str(self.__height) + ")"
+            return (rectangle)
+
         def __del__(self):
             Rectangle.number_of_instances -= 1
-            print("Bye rectangle" + "..." * 3)
+            print("Bye rectangle...")
